3bacb5d2e1f4c3a1315567206b924e15655ce2c7
nonaninona <starcraft0529@gmail.com>
2024-09-20 18:41:07 +0900

    Row 테스트 작성

---
+++ 이현희/untitled/src/test/java/RowTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+public class RowTest {
+    @Test
+    public void 행만들기() {
+
+    }
+
+    @Test
+    public void 행에선그리기() {
+
+    }
+
+    @Test
+    public void 행에서실행하기() {
+
+    }
+
+    @Test
+    public void 행출력하기() {
+
+    }
+}
