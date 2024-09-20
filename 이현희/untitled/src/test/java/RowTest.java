3bdd5b863b51de6ea7a0425246e220952fcd3685
nonaninona <starcraft0529@gmail.com>
2024-09-20 18:44:56 +0900

    Row 테스트 구체적으로 작성

--- 이현희/untitled/src/test/java/RowTest.java
+++ 이현희/untitled/src/test/java/RowTest.java
@@ -1,23 +1,51 @@
 import org.junit.jupiter.api.Test;
 
-public class RowTest {
-    @Test
-    public void 행만들기() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    }
+public class RowTest {
 
     @Test
     public void 행에선그리기() {
+        //given
+        int size = 5;
+        Row row = new Row(size);
 
+        //when
+        int position = 3;
+        row.drawLine(position);
+
+        //then
+        assertEquals(row.isLineExist(position), true);
     }
 
     @Test
     public void 행에서실행하기() {
+        //given
+        int size = 5;
+        Row row = new Row(size);
+        int drawPosition = 3;
+        row.drawLine(drawPosition);
 
+        //when
+        int startPosition = 3;
+        int endPosition = row.run(startPosition);
+
+        //then
+        assertEquals(endPosition, 4);
     }
 
     @Test
     public void 행출력하기() {
+        //given
+        int size = 5;
+        Row row = new Row(size);
+        int position = 3;
+        row.drawLine(position);
 
+        //when
+        String str = row.print();
+
+        //then
+        assertEquals(str, "0 0 1 0 0");
     }
 }
