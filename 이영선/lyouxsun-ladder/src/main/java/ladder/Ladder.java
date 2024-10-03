6ec7ee48750c0adc1cfbd9c9d49b3c1ce41b31fe
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-10-03 13:45:17 +0900

    feat: 객체지향 설계 TODO list 작성

---
+++ 이영선/lyouxsun-ladder/src/main/java/ladder/Ladder.java
@@ -0,0 +1,11 @@
+package ladder;
+
+public class Ladder {
+
+    private final int[][] rows;
+
+    public Ladder(int row, int numberOfPerson) {
+        rows = new int[row][numberOfPerson];
+    }
+
+}
