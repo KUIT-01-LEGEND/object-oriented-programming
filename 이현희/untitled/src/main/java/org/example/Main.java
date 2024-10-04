309bef6b6b72af5f23ba608ef230c55a92c370fd
nonaninona <starcraft0529@gmail.com>
2024-10-04 12:37:35 +0900

    feat : 메시지 1 구현 중 수정 -> 포함관계는 리턴을 하는 게 아니고 생성자로 리턴이 없도록 구성하는 게 맞는 듯

--- 이현희/untitled/src/main/java/org/example/Main.java
+++ 이현희/untitled/src/main/java/org/example/Main.java
@@ -2,39 +2,57 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user = new User();
+        Game game = new Game();
+
+        user.makeLadder(game, 3, 3);
     }
 }
 
 
 class User {
-    public void makeLadder(int width, int height) {}
+    public void makeLadder(Game game, int width, int height) {
+        game.makeLadder(width, height);
+    }
     public void drawHorizontalLine(int row, int column) {}
     public int runGame() { return -1; }
 }
 
 class Game {
-    public void makeLadder(int width, int height) {}
+    Ladder ladder = new Ladder();
+    public void makeLadder(int width, int height) {
+        ladder.makeLadder(width, height);
+    }
     public void drawHorizontalLine(int row, int column) {}
     public int runGame() { return -1; }
 }
 
 class Ladder {
-    public void makeLadder(int width, int height) {}
+    Column[] columns;
+    public void makeLadder(int width, int height) {
+        columns = new Column[width];
+        for (int i = 0; i < width; i++) {
+            columns[i] = new Column(height);
+        }
+    }
     public void drawHorizontalLine(int row, int column) {}
     public int runGame() { return -1; }
 }
 
 class Column {
-    public Column[] makeColumns(int width) {
-        return null;
+    Row[] rows;
+    public Column(int height) {
+        rows = new Row[height];
+        for (int i = 0; i < rows.length; i++) {
+            rows[i] = new Row();
+        }
     }
     public void drawHorizontalLine(int row) {}
 }
 
 class Row {
-    public Row[] makeRows(int height) {
-        return null;
+    public Row() {
+
     }
     public void drawHorizontalLine() {}
 }
