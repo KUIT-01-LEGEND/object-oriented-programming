693c688f722ef79c63de4b82a919766e25634e26
nonaninona <starcraft0529@gmail.com>
2024-10-04 10:44:34 +0900

    feat : 3번 메시지에 대한 협력 관계 구현
    3번 메시지 - 가로줄을 그어라

--- 이현희/untitled/src/main/java/org/example/Main.java
+++ 이현희/untitled/src/main/java/org/example/Main.java
@@ -9,24 +9,29 @@ public class Main {
 
 class User {
     public void makeLadder(int width, int height) {}
+    public void drawHorizontalLine(int row, int column) {}
 }
 
 class Game {
     public void makeLadder(int width, int height) {}
+    public void drawHorizontalLine(int row, int column) {}
 }
 
 class Ladder {
     public void makeLadder(int width, int height) {}
+    public void drawHorizontalLine(int row, int column) {}
 }
 
 class Column {
     public Column[] makeColumns(int width) {
         return null;
     }
+    public void drawHorizontalLine(int row) {}
 }
 
 class Row {
     public Row[] makeRows(int height) {
         return null;
     }
+    public void drawHorizontalLine() {}
 }
